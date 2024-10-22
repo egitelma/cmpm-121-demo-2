db555bebf8f0d72b0176dc5d58d7014a9729794c
egitelma <egitelma@ucsc.edu>
2024-10-22 16:42:14 -0700

    Step 3 complete: Pretty sure I implemented an observer properly, plus Point interface.

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ document.title = APP_NAME;
 heading.innerHTML = APP_NAME;
 canvas.id = "canvas";
 
+//Point interface
+interface Point {
+    x: number,
+    y: number
+}
+
 //Establishing some canvas variables
 let width = canvas.width = 256;
 let height = canvas.height = 256;
@@ -20,7 +26,9 @@ if(ctx != null){
     ctx.fillRect(0, 0, width, height);
 }
 let isDrawing = false;
+let activeLine : Point[] = [];
 let x : number, y : number = 0;
+let points_arr : Point[][] = [];
 
 //Adding into the DOM
 app.append(heading);
@@ -28,33 +36,61 @@ app.append(canvas);
 
 //Functions - I snagged these off the mousemove documentation: https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
 canvas.addEventListener("mousedown", (e) => {
-  x = e.offsetX;
-  y = e.offsetY;
-  isDrawing = true;
+    x = e.offsetX;
+    y = e.offsetY;
+    activeLine = [];
+    points_arr.push(activeLine);
+    isDrawing = true;
 });
 canvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
-    drawLine(ctx, x, y, e.offsetX, e.offsetY);
+    addNewPt(x, y);
     x = e.offsetX;
     y = e.offsetY;
   }
 });
 canvas.addEventListener("mouseup", (e) => { //interestingly, in the example, this one uses window instead of canvas. Canvas works just fine though.
   if (isDrawing) {
-    drawLine(ctx, x, y, e.offsetX, e.offsetY);
-    x = 0;
-    y = 0;
+    activeLine.push({x, y});
+    drawingChanged(ctx);
     isDrawing = false;
   }
 });
 
 function drawLine(context, x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = "black";
-  context.lineWidth = 1;
   context.moveTo(x1, y1);
   context.lineTo(x2, y2);
   context.stroke();
   context.closePath();
 }
 
+function drawingChanged(context){
+    clearCanvas(context);
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    redrawPts(context);
+}
+
+function clearCanvas(context){
+    context.clearRect(0, 0, width, height);
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+}
+
+function redrawPts(context){
+    for(let activeLn of points_arr){
+        let lastX = activeLn[0].x;
+        let lastY = activeLn[0].y; //I know there's a better way to write these two lines but ARGH I can't remember what it was from lecture.
+        for(let {x, y} of activeLn){ //Redraw all points
+            drawLine(context, lastX, lastY, x, y);
+            lastX = x;
+            lastY = y;
+        }
+    }
+}
+
+function addNewPt(new_x : number, new_y : number){
+    activeLine.push({x: new_x, y: new_y});
+    drawingChanged(ctx);
+}
