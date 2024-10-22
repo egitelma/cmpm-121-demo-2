5ccc08ea0f62ada63fbd31aa8c8e4976a37afd1b
egitelma <egitelma@ucsc.edu>
2024-10-21 19:50:43 -0700

    Step 2 complete: drawing on canvas, plus some organization

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,60 @@
 import "./style.css";
 
+//Const/page element creation
 const APP_NAME = "Painting Canvas";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
-document.title = APP_NAME;
-// app.innerHTML = APP_NAME;
-
-//Step 1: h1 element, canvas, canvas black border
 const heading = document.createElement("h1");
-heading.innerHTML = APP_NAME;
-app.append(heading);
-
 const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+//Defining properties
+document.title = APP_NAME;
+heading.innerHTML = APP_NAME;
 canvas.id = "canvas";
+
+//Establishing some canvas variables
 let width = canvas.width = 256;
 let height = canvas.height = 256;
-const ctx = canvas.getContext("2d");
 if(ctx != null){
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, width, height);
 }
+let isDrawing = false;
+let x : number, y : number = 0;
+
+//Adding into the DOM
+app.append(heading);
 app.append(canvas);
+
+//Functions - I snagged these off the mousemove documentation: https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(ctx, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+canvas.addEventListener("mouseup", (e) => { //interestingly, in the example, this one uses window instead of canvas. Canvas works just fine though.
+  if (isDrawing) {
+    drawLine(ctx, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
