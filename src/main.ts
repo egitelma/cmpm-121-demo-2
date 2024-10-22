3b9825b77c944f18e6dd38697b02432d870fde8f
egitelma <egitelma@ucsc.edu>
2024-10-21 19:17:50 -0700

    Step 1 complete: added an h1, canvas, plus a nice drop shadow

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import "./style.css";
 
-const APP_NAME = "Test";
+const APP_NAME = "Painting Canvas";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// app.innerHTML = APP_NAME;
+
+//Step 1: h1 element, canvas, canvas black border
+const heading = document.createElement("h1");
+heading.innerHTML = APP_NAME;
+app.append(heading);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+let width = canvas.width = 256;
+let height = canvas.height = 256;
+const ctx = canvas.getContext("2d");
+if(ctx != null){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, width, height);
+}
+app.append(canvas);
