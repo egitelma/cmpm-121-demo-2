07cb09bcdb4a01b0c58d41b8bcaf61c5df5b03d0
egitelma <egitelma@ucsc.edu>
2024-10-26 16:56:33 -0700

    Step 5 complete: rewrote around the new Line class

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,49 @@ undo_btn.innerHTML = "undo";
 redo_btn.innerHTML = "redo";
 btn_div.id = "btn_div";
 
+//Adding into the DOM
+app.append(heading);
+app.append(canvas);
+app.append(btn_div);
+btn_div.append(undo_btn);
+btn_div.append(redo_btn);
+
 //Point interface
 interface Point {
     x: number,
-    y: number
+    y: number,
+}
+
+//Thought we're not meant to have classes in this, I guess we're ditching that here? (For step 5, which explicitly asks for a class)
+class Line {
+    initial_x : number;
+    initial_y : number;
+    end_x : number;
+    end_y : number;
+    points_arr: Point[];
+    constructor(start_x : number, start_y : number){
+        let new_pt : Point = {x: start_x, y: start_y}
+        this.points_arr = [];
+        this.points_arr.push(new_pt);
+    }
+    display(ctx : CanvasRenderingContext2D){
+        let last_pt : Point = this.points_arr[0];
+        for(let current_pt of this.points_arr){
+            this.drawLine(ctx, last_pt, current_pt); //wow that's WAY cleaner.
+            last_pt = current_pt;
+        }
+    }
+    drawLine(ctx: CanvasRenderingContext2D, point1: Point, point2: Point){
+        ctx.beginPath();
+        ctx.moveTo(point1.x, point1.y);
+        ctx.lineTo(point2.x, point2.y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+    drag(new_x : number, new_y : number){
+        let new_pt : Point = {x: new_x, y: new_y};
+        this.points_arr.push(new_pt);
+    }
 }
 
 //Establishing some canvas variables
@@ -32,38 +71,31 @@ if(ctx != null){
     ctx.fillRect(0, 0, width, height);
 }
 let isDrawing = false;
-let activeLine : Point[] = [];
 let x : number, y : number = 0;
-let points_arr : Point[][] = [];
-let redo_stack : Point[][] = [];
-
-//Adding into the DOM
-app.append(heading);
-app.append(canvas);
-app.append(btn_div);
-btn_div.append(undo_btn);
-btn_div.append(redo_btn);
-
+let activeLine : Line;
+let lines_arr : Line[] = [];
+let redo_stack : Line[] = [];
 
 //Functions - I snagged these off the mousemove documentation: https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
 canvas.addEventListener("mousedown", (e) => {
     x = e.offsetX;
     y = e.offsetY;
-    activeLine = [];
-    points_arr.push(activeLine);
+    activeLine = new Line(x, y);
+    lines_arr.push(activeLine);
     isDrawing = true;
     if(redo_stack.length > 0) redo_stack = [];
 });
 canvas.addEventListener("mousemove", (e) => {
   if (isDrawing) {
-    addNewPt(x, y);
+    activeLine.drag(x, y);
+    drawingChanged(ctx);
     x = e.offsetX;
     y = e.offsetY;
   }
 });
 canvas.addEventListener("mouseup", (e) => { //interestingly, in the example, this one uses window instead of canvas. Canvas works just fine though.
   if (isDrawing) {
-    activeLine.push({x, y});
+    activeLine.drag(x, y);
     drawingChanged(ctx);
     isDrawing = false;
   }
@@ -75,14 +107,6 @@ undo_btn.addEventListener("click", (e) => {
 redo_btn.addEventListener("click", (e) => {
     redo();
 })
-
-function drawLine(context, x1, y1, x2, y2) {
-  context.beginPath();
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.stroke();
-  context.closePath();
-}
 
 function drawingChanged(context){
     clearCanvas(context);
@@ -98,34 +122,23 @@ function clearCanvas(context){
 }
 
 function redrawPts(context){
-    for(let activeLn of points_arr){
-        let lastX = activeLn[0].x;
-        let lastY = activeLn[0].y; //I know there's a better way to write these two lines but ARGH I can't remember what it was from lecture.
-        for(let {x, y} of activeLn){
-            drawLine(context, lastX, lastY, x, y);
-            lastX = x;
-            lastY = y;
-        }
+    for(let line of lines_arr){
+        line.display(context);
     }
 }
 
-function addNewPt(new_x : number, new_y : number){
-    activeLine.push({x: new_x, y: new_y});
-    drawingChanged(ctx);
-}
-
 function undo(){
-    let last_pt = points_arr.pop();
-    if(last_pt != undefined){
-        redo_stack.push(last_pt);
+    let last_line = lines_arr.pop();
+    if(last_line != undefined){
+        redo_stack.push(last_line);
         drawingChanged(ctx);
     }
 }
 
 function redo(){
-    let last_pt = redo_stack.pop();
-    if(last_pt != undefined){
-        points_arr.push(last_pt);
+    let last_line = redo_stack.pop();
+    if(last_line != undefined){
+        lines_arr.push(last_line);
         drawingChanged(ctx);
     }
 }
